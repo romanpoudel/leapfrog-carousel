98a241e291c73bb30fb7dd6276eed923841eb1f5
romanpoudel <romanpoudel325@gmail.com>
2023-11-29 22:59:30 +0545

    circle animate

--- script.js
+++ script.js
@@ -14,44 +14,69 @@ for (let i = 0; i < carouselImg.length; i++) {
 	document.querySelector(".carousel__nav").appendChild(circle);
 }
 
+//animate circles
+function animateCircles() {
+	const circles = document.querySelectorAll(".circle");
+	circles.forEach(function (circle, index) {
+		if (index === carouselImgIndex) {
+			circle.classList.add("circle--active");
+		} else {
+			circle.classList.remove("circle--active");
+		}
+	});
+}
 //for making the navigation circles work
 document.addEventListener("click", (e) => {
-    if(e.target.classList.contains("circle")){
-        carouselImgIndex=e.target.getAttribute("index");
-        moveRight();
-    }
+	if (e.target.classList.contains("circle")) {
+		carouselImgIndex = e.target.getAttribute("index");
+		moveRight();
+	}
 });
 
 // move left function
 function moveLeft() {
-    carouselWrapper.style.left =450-carouselImg.length*450 + 450 * carouselImgIndex + "px";
-    console.log(carouselImgIndex)
+	carouselWrapper.style.left =
+		450 - carouselImg.length * 450 + 450 * carouselImgIndex + "px";
+	console.log(carouselImgIndex);
+	//animate circles
+    animateCircles();
 }
 
 leftArrow.addEventListener("click", () => {
 	console.log("left");
 	if (carouselImgIndex > 0) {
 		carouselImgIndex--;
-	}else{
-        carouselImgIndex=carouselImg.length-1;
-    }
+	} else {
+		carouselImgIndex = carouselImg.length - 1;
+	}
 	moveLeft();
 });
 
 //move right function
 function moveRight() {
 	carouselWrapper.style.left = -450 * carouselImgIndex + "px";
-    console.log(carouselImgIndex)
+	console.log(carouselImgIndex);
+	//animate circles
+    animateCircles();
 }
 
 rightArrow.addEventListener("click", () => {
 	console.log("right");
 	if (carouselImgIndex < carouselImg.length - 1) {
 		carouselImgIndex++;
-	}else{
-        carouselImgIndex=0;
-    }
+	} else {
+		carouselImgIndex = 0;
+	}
 	moveRight();
 });
 
 
+//automatically animate or slide carousel images
+setInterval(() => {
+	if (carouselImgIndex < carouselImg.length - 1) {
+		carouselImgIndex++;
+	} else {
+		carouselImgIndex = 0;
+	}
+	moveRight();
+}, 3000);
